a83ea04b8abc9db6ca1fbd5a6ca2e73183965ec8
LeoSilvax6 <leo_jsilva2015@hotmail.com>
2026-02-26 15:52:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# Calculadora simples - Versão 2
+# Calculadora simples - Versão 3
 
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
 print("Soma:", num1 + num2)
 print("Subtração:", num1 - num2)
+print("Multiplicação:", num1 * num2)
